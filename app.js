3231da4cfff7fc25a1fc9c99ba42289d6d9cdf9f
Stu Penrose <stu@penrose.us>
2022-07-19 14:41:49 -0700

    Example from day 2

--- app.js
+++ app.js
@@ -7,5 +7,28 @@ button.onclick = function(){
     let intro = introField.value
     let message = "I can't believe you said " + intro
     console.log(message)
-    document.write(message)
+
+    const sendMessageResponseArea = document.querySelector('.response-to-send-message')
+
+    sendMessageResponseArea.textContent = message
+}
+
+console.log("document is ", document)
+const stopButton = document.querySelector('.stop-button')
+const startButton = document.querySelector('.start-button')
+
+// startButton.innerText = "fooo"
+// startButton.style["background"] = "green"
+
+startButton.onclick = function() {
+    const buttonMessage = document.querySelector('.start-message')
+    buttonMessage.textContent = "santa cruz.  how you doin?"
+    buttonMessage.style["background"] = "green"
+    buttonMessage.style["color"] = "white"
+}
+
+stopButton.onclick = function() {
+
+    const otherMessage = document.querySelector('.end-message')
+    otherMessage.textContent = "teenagers smell"
 }
