c998e6122dc88ec5524ae61a496b1a9698815395
Stu Penrose <stu@penrose.us>
2022-08-01 13:16:57 -0700

    Updated demo for today

--- app.js
+++ app.js
@@ -1,82 +1,95 @@
 console.log("Hello world")
 
+let loadingPanel = document.querySelector(".loading-panel")
+let introPanel = document.querySelector(".intro-panel")
+let gamePanel = document.querySelector(".game-panel")
+let userNameArea = gamePanel.querySelector(".user-name-area")
 let introField = document.querySelector(".intro-field")
-const sendMessageResponseArea = document.querySelector('.response-to-send-message')
-let button = document.querySelector("button")
-let player = document.querySelector(".person")
+let choosePlayerAButton = document.querySelector(".choose-player-a-button")
+let choosePlayerBButton = document.querySelector(".choose-player-b-button")
+let playerA = document.querySelector(".person-a")
+let playerB = document.querySelector(".person-b")
 
-button.onclick = function(){
-    let intro = introField.value
-    let message = "I can't believe you said " + intro
-    console.log(message)
-
-
-    sendMessageResponseArea.textContent = message
-}
-
-console.log("document is ", document)
-const stopButton = document.querySelector('.stop-button')
-const startButton = document.querySelector('.start-button')
-
-// startButton.innerText = "fooo"
-// startButton.style["background"] = "green"
-
-startButton.onclick = function() {
-    const buttonMessage = document.querySelector('.start-message')
-    buttonMessage.textContent = "santa cruz.  how you doin?"
-    buttonMessage.style["background"] = "green"
-    buttonMessage.style["color"] = "white"
-}
-
-stopButton.onclick = function() {
-    const otherMessage = document.querySelector('.end-message')
-    otherMessage.textContent = "teenagers smell"
-}
-
-let position = window.visualViewport.width / 2
-let velocity = 0
+let playerSlot;
+let player;
+let otherPlayer;
 
 
-let showVelocityWarnings = ()=>{
-    let velocityMessage
-    let velocityMessageColor
+holler.onLoad(()=>{
 
-    if(velocity>5 || velocity < -5){
-        velocityMessage = "Hyper speed!"
-        velocityMessageColor = "red"
-    }else if(velocity>2 || velocity < -2){
-        velocityMessage = "Speed demon!"
-        velocityMessageColor = "yellow"
-    }else{
-        velocityMessage = ""
-        velocityMessageColor = "none"
+    holler.me((user)=>{
+        loadingPanel.style.display="none"
+        introPanel.style.display="block"
+        console.log("user stuff is", user)
+        userNameArea.textContent = user.name
+    })
+
+    holler.onClientEvent(event=>{
+        console.log(`Client event received: ${event}`)
+        if(event.indexOf(playerSlot) == -1){
+            let newPositionOtherPlayer = parseInt(event)
+            setPosition(otherPlayer, newPositionOtherPlayer)
+        }
+    })
+
+    function startGame(chosenPlayer){
+        playerSlot = chosenPlayer
+        if(playerSlot == "a"){
+            player = playerA
+            otherPlayer = playerB
+        }else{
+            player = playerB
+            otherPlayer = playerA
+        }
+        gamePanel.style.display = "block"
+        introPanel.style.display = "none"
+        console.log("player is", playerSlot, player)
     }
-    sendMessageResponseArea.textContent = velocityMessage
-    sendMessageResponseArea.style["background"] = velocityMessageColor
-}
 
-document.onkeydown = (keyEvent)=>{
-    console.log("Keypressed: " + keyEvent.key)
-    switch(keyEvent.key){
-        case "a": 
-        case "ArrowLeft":
-            velocity = velocity -1
-            break;
-        case "d": 
-        case "ArrowRight":
-            velocity = velocity +1
-            break;
+    choosePlayerAButton.onclick = function(){
+        startGame("a")
     }
-    console.log("Current velocity is", velocity)
 
-    showVelocityWarnings()
-}
+    choosePlayerBButton.onclick = function(){
+        startGame("b")
+    }
 
+    let position = window.visualViewport.width / 2
+    let velocity = 0
 
-const doNextFrame = ()=>{
-    position = position + velocity
-    player.style.left = position + "px"
-    setTimeout(doNextFrame)
-}
+    document.onkeydown = (keyEvent)=>{
+        console.log("Keypressed: " + keyEvent.key)
+        switch(keyEvent.key){
+            case "a": 
+            case "ArrowLeft":
+                velocity = velocity -1
+                break;
+            case "d": 
+            case "ArrowRight":
+                velocity = velocity +1
+                break;
+        }
+        console.log("Current velocity is", velocity)
 
-doNextFrame()
+    }
+
+    function setPosition(player, position){
+        if(player){
+            player.style.left = position + "px"
+        }
+    }
+
+    const doNextFrame = ()=>{
+        let oldPosition = position
+        position = position + velocity
+        setPosition(player, position)
+
+        if(oldPosition!==position){
+            holler.appInstance.notifyClients(position + " " + playerSlot)
+        }
+        setTimeout(doNextFrame)
+    }
+
+    doNextFrame()
+})
+
