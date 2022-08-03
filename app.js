790bdba1e1917f44a896a0c1b71670fa9ee5383b
Stu Penrose <stu@penrose.us>
2022-08-02 17:25:17 -0700

    Demo for more than 2 players

--- app.js
+++ app.js
@@ -7,81 +7,113 @@ let userNameArea = gamePanel.querySelector(".user-name-area")
 let introField = document.querySelector(".intro-field")
 let choosePlayerAButton = document.querySelector(".choose-player-a-button")
 let choosePlayerBButton = document.querySelector(".choose-player-b-button")
-let playerA = document.querySelector(".person-a")
-let playerB = document.querySelector(".person-b")
 
-let playerSlot;
+let availableImages = [
+    "person1.gif",
+    "person2.gif",
+    "person3.gif",
+    "person4.gif",
+]
+
+function makeCharacter(id, image){
+    let imageTag = document.createElement("img")
+    imageTag.src = image
+    imageTag.setAttribute("class", "person")
+
+    let character = {
+        id:id,
+        imageName:image,
+        image:imageTag,
+        velocity:0,
+        position:(window.visualViewport.width / 2)
+    }
+
+    gamePanel.appendChild(imageTag)
+    characters.push(character)
+
+    return character
+}
+
 let player;
-let otherPlayer;
-
+let characters = [];
+let myName = "??"
 
 holler.onLoad(()=>{
 
     holler.me((user)=>{
         loadingPanel.style.display="none"
         introPanel.style.display="block"
+
+        availableImages.forEach(image=>{
+            let button = document.createElement("img")
+            button.classList = "character-option"
+            button.src = image
+
+            button.onclick = ()=>{
+                startGame(image)
+            }
+
+            introPanel.appendChild(button)
+        })
+
         console.log("user stuff is", user)
         userNameArea.textContent = user.name
+        myName = user.name
     })
 
-    holler.onClientEvent(event=>{
-        console.log(`Client event received: ${event}`)
-        if(event.indexOf(playerSlot) == -1){
-            let newPositionOtherPlayer = parseInt(event)
-            setPosition(otherPlayer, newPositionOtherPlayer)
+    holler.onClientEvent(message=>{
+        console.log(`Client event received: ${message}`)
+        let playerUpdate = JSON.parse(message)
+
+        if(playerUpdate.id != player?.id){
+
+            let otherPlayer = characters.find(o=>o.id == playerUpdate.id)
+            if(!otherPlayer){
+                otherPlayer = makeCharacter(playerUpdate.id, playerUpdate.imageName)
+            }
+
+            otherPlayer.position = playerUpdate.position
+            otherPlayer.velocity = playerUpdate.velocity
+            setPosition(otherPlayer)
         }
     })
 
-    function startGame(chosenPlayer){
-        playerSlot = chosenPlayer
-        if(playerSlot == "a"){
-            player = playerA
-            otherPlayer = playerB
-        }else{
-            player = playerB
-            otherPlayer = playerA
-        }
+    function startGame(imageName){
+        player = makeCharacter(myName + "-" + imageName, imageName)
         gamePanel.style.display = "block"
         introPanel.style.display = "none"
-        console.log("player is", playerSlot, player)
+        console.log("player is", player)
     }
-
-    choosePlayerAButton.onclick = function(){
-        startGame("a")
-    }
-
-    choosePlayerBButton.onclick = function(){
-        startGame("b")
-    }
-
-    let position = window.visualViewport.width / 2
-    let velocity = 0
 
     document.onkeydown = (keyEvent)=>{
         console.log("Keypressed: " + keyEvent.key)
         switch(keyEvent.key){
             case "a": 
             case "ArrowLeft":
-                velocity = velocity -1
+                player.velocity = player.velocity -1
                 break;
             case "d": 
             case "ArrowRight":
-                velocity = velocity +1
+                player.velocity = player.velocity +1
                 break;
         }
-        console.log("Current velocity is", velocity)
+        console.log("Current velocity is", player.velocity)
 
     }
 
-    function setPosition(player, position){
+    function setPosition(player){
         if(player){
-            player.style.left = position + "px"
+            player.image.style.left = player.position + "px"
         }
     }
 
     const doNextFrame = ()=>{
-        position = position + velocity
-        setPosition(player, position)
+        
+        characters.forEach(character=>{
+            character.position = character.position + character.velocity
+            setPosition(character)
+        })
+
         setTimeout(doNextFrame)
     }
 
@@ -90,8 +122,15 @@ holler.onLoad(()=>{
 
 
     const sendPosition = ()=>{
-        holler.appInstance.notifyClients(position + " " + playerSlot)
-        setTimeout(sendPosition, 100)
+        if(player){
+            holler.appInstance.notifyClients(JSON.stringify({
+                id:player.id,
+                position:player.position,
+                velocity:player.velocity,
+                imageName:player.imageName
+            }))
+        }
+        setTimeout(sendPosition, 10)
     }
     sendPosition()
 })
